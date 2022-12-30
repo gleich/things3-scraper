74e0a2dd8f6c06bd991f6cc31ec74972fb7bfbb9
Matt Gleich <git@mattglei.ch>
2022-12-29 23:56:55 -0500

    feat: parse config
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/config.rs
@@ -0,0 +1,66 @@
+use anyhow::{Context, Result};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Config {
+    pub endpoint: String,
+    #[serde(default = "defaults::refresh")]
+    pub refresh: u32,
+    #[serde(default)]
+    pub bearer_token: String,
+}
+
+mod defaults {
+    pub fn refresh() -> u32 {
+        10
+    }
+}
+
+impl Config {
+    pub fn parse<T: ToString>(content: T) -> Result<Self> {
+        let decoded: Self =
+            toml::from_str(&content.to_string()).context("failed to decode config toml")?;
+        Ok(decoded)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Config;
+
+    #[test]
+    fn parsing() {
+        // basic valid config
+        assert_eq!(
+            Config::parse(
+                r#"
+        endpoint = "https://google.com/"
+        "#,
+            )
+            .expect("failed to parse valid basic config"),
+            Config {
+                endpoint: String::from("https://google.com/"),
+                refresh: 10,
+                bearer_token: String::new()
+            }
+        );
+        // complex valid config
+        assert_eq!(
+            Config::parse(
+                r#"
+        endpoint = "https://google.com/"
+        refresh = 20
+        bearer_token = "token"
+        "#,
+            )
+            .expect("failed to parse valid basic config"),
+            Config {
+                endpoint: String::from("https://google.com/"),
+                refresh: 20,
+                bearer_token: String::from("token")
+            }
+        );
+        // invalid config
+        assert!(Config::parse("").is_err());
+    }
+}
